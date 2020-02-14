f7c32c40b1bc957f1eb3493fb03420c4bf25d2fe
Leo <Leobrac23@gmail.com>
2020-02-13 23:07:47 -0300

    Update for 1.15.2

--- src/main/java/mezz/nopotionshift/NoPotionShift.java
+++ src/main/java/mezz/nopotionshift/NoPotionShift.java
@@ -2,25 +2,17 @@ package mezz.nopotionshift;
 
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(
-		modid = "nopotionshift",
-		version = "@VERSION@",
-		acceptedMinecraftVersions = "[1.10,)",
-		clientSideOnly = true
-)
+@Mod("nopotionshift")
 public class NoPotionShift {
-	@EventHandler
-	public void init(FMLInitializationEvent event) {
+	public NoPotionShift() {
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	@SubscribeEvent
-	public void onPotionShiftEvent(GuiScreenEvent.PotionShiftEvent event) {
+	public void hasPotionGui(GuiScreenEvent.PotionShiftEvent event) {
 		event.setCanceled(true);
 	}
 }
