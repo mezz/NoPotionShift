6c93c41754098fe38aac430d7ae1a9968480c9dc
Kalka <kalka2088@gmail.com>
2021-05-17 00:18:49 -0400

    Improve client-only compatibility (#7)

--- src/main/java/mezz/nopotionshift/NoPotionShift.java
+++ src/main/java/mezz/nopotionshift/NoPotionShift.java
@@ -1,13 +1,24 @@
 package mezz.nopotionshift;
 
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("nopotionshift")
 public class NoPotionShift {
 	public NoPotionShift() {
+		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> this::registerClientEvent);
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+	}
+
+	public void registerClientEvent() {
 		MinecraftForge.EVENT_BUS.addListener(
 				EventPriority.NORMAL,
 				false,
