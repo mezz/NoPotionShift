3ab848a656c1f26987a0c32d66c71710b136591c
Kalka <k@kalka.io>
2021-09-02 18:53:38 -0400

    Port to 1.17

--- src/main/java/mezz/nopotionshift/NoPotionShift.java
+++ src/main/java/mezz/nopotionshift/NoPotionShift.java
@@ -5,20 +5,20 @@ import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
-import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("nopotionshift")
 public class NoPotionShift {
 	public NoPotionShift() {
-		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> this::registerClientEvent);
-		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> PotionShiftHandler::registerClientEvent);
+		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "ANY", (remote, side) -> side));
 	}
+}
 
-	public void registerClientEvent() {
+class PotionShiftHandler {
+	public static void registerClientEvent() {
 		MinecraftForge.EVENT_BUS.addListener(
 				EventPriority.NORMAL,
 				false,
